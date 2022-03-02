af23cd8bcf913cdfcab62c888164bcc29c7164b0
AnbaiAnbai <97570265+AnbaiAnbai@users.noreply.github.com>
2022-03-02 11:53:10 +0900

    design change

--- src/page/Report.js
+++ src/page/Report.js
@@ -1,7 +1,23 @@
+import { Col, Row } from "react-bootstrap";
 import { withRouter } from "react-router";
 
 const Report = () => {
-    return <h1>This is report mode.</h1>
+    return (
+        <Row xs="auto">
+            <Col><h4>レポート:</h4></Col>
+            <Col>
+                <div style={{backgroundColor: "black", width:"1000px", height:"1000px"}}>
+                    <ul style={{listStyleType:"none", color : "white"}}>
+                        <li>1_disp-coias-nonmask.png</li>
+                        <li>2_disp-coias-nonmask.png</li>
+                        <li>3_disp-coias-nonmask.png</li>
+                        <li>4_disp-coias-nonmask.png</li>
+                        <li>5_disp-coias-nonmask.png</li>
+                    </ul>
+                </div>     
+            </Col>
+        </Row>
+    )
 }
 
 export default withRouter(Report);
