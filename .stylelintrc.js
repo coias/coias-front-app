2271f992ba447c266b0a5be27055ddae517dcf5c
Minaya Usuki <usuki@aizulab.com>
2022-03-08 13:11:27 +0900

    stylelintの導入

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-standard-scss',
+  ],
   plugins: ['stylelint-order'],
   ignoreFiles: ['**/node_modules/**'],
   rules: {
