6f629d145a2450a7631ca75011824005f1ad7082
“AnbaiAnbai” <anbai@aizulab.com>
2022-03-23 13:47:54 +0900

    #2 fix

--- src/page/Report.jsx
+++ src/page/Report.jsx
@@ -1,42 +1,116 @@
-import React, { useContext } from 'react';
-import { Col, Row } from 'react-bootstrap';
+import React, { useMemo, useState } from 'react';
+import axios from 'axios';
+import { Col, Row, Form, Button } from 'react-bootstrap';
 import { Scrollbars } from 'react-custom-scrollbars';
-import { StarPositionContext } from '../component/context';
 
 function Report() {
-  const { starPos } = useContext(StarPositionContext);
+  const reactApiUri = process.env.REACT_APP_API_URI;
+
+  const [sendMpcOBS, setSendMpcOBS] = useState('');
+  const [sendMpcMEA, setSendMpcMEA] = useState('');
+  const [sendMpcBody, setSendMpcBody] = useState([]);
+  const [sendMpc, setSendMpc] = useState([]);
+
+  const makeSendMpc = () => {
+    const header = [
+      'COD 568',
+      'CON S. Urakawa, Bisei Spaceguard Center, 1716-3, Okura, Bisei',
+      'CON Ibara, 714-1411 Okayama, Japan [urakawa@spaceguard.or.jp]',
+      `OBS ${sendMpcOBS}`,
+      `MEA ${sendMpcMEA}`,
+      'TEL 8.2-m f/2.0 reflector + CCD(HSC)',
+      'NET Pan-STARRS(PS1) DR1 catalog',
+      'ACK Subaru/HSC',
+    ];
+
+    setSendMpc(header.concat(sendMpcBody));
+  };
+
+  const getMpc = async () => {
+    const response = await axios.put(`${reactApiUri}AstsearchR_afterReCOIAS`);
+    const mpctext = await response.data.send_mpc;
+    const re = /((?!(H|K))(.*))/g;
+    const result = mpctext.split(re);
+    setSendMpcBody(result);
+  };
+  // 初回のみのAPIの読み込み
+  useMemo(() => {
+    getMpc();
+  }, []);
 
   return (
-    <Row xs="auto">
-      <Col>
-        <h4>レポート:</h4>
-      </Col>
-      <Col>
-        <Scrollbars
-          style={{
-            backgroundColor: 'black',
-            width: '1000px',
-            height: '1000px',
-          }}
-        >
-          <div
+    <div>
+      <Form>
+        <Form.Group className="p-3 w-75">
+          <Row xs="auto">
+            <Col md={1}>
+              <h4>OBS:</h4>
+            </Col>
+            <Col md={10}>
+              <Form.Control
+                placeholder="複数の場合は空白区切りで入力してください"
+                onChange={(e) => {
+                  setSendMpcOBS(e.target.value);
+                }}
+              />
+            </Col>
+          </Row>
+        </Form.Group>
+        <Form.Group className="p-3 w-75">
+          <Row xs="auto">
+            <Col md={1}>
+              <h4>MEA:</h4>
+            </Col>
+            <Col md={10}>
+              <Form.Control
+                placeholder="複数の場合は空白区切りで入力してください"
+                onChange={(e) => {
+                  setSendMpcMEA(e.target.value);
+                }}
+              />
+            </Col>
+            <Col md={1}>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  makeSendMpc();
+                }}
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form.Group>
+      </Form>
+      <Row xs="auto">
+        <Col>
+          <h4>レポート:</h4>
+        </Col>
+        <Col>
+          <Scrollbars
             style={{
               backgroundColor: 'black',
               width: '1000px',
               height: '1000px',
             }}
           >
-            <ul style={{ listStyleType: 'none', color: 'white' }}>
-              {starPos
-                .filter((pos) => pos[4])
-                .map((pos) => (
-                  <li key={pos[2]}>{pos}</li>
+            <div
+              style={{
+                backgroundColor: 'black',
+                width: '1000px',
+                height: '1000px',
+              }}
+            >
+              <ul style={{ listStyleType: 'none', color: 'white' }}>
+                {sendMpc.map((arr) => (
+                  <li key={arr}>{arr}</li>
                 ))}
-            </ul>
-          </div>
-        </Scrollbars>
-      </Col>
-    </Row>
+              </ul>
+            </div>
+          </Scrollbars>
+        </Col>
+      </Row>
+    </div>
   );
 }
 
