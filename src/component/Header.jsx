12e2c29df421a3fa31789017f7f8f19014968501
“AnbaiAnbai” <anbai@aizulab.com>
2022-03-28 17:01:38 +0900

    fix : Header 微調整

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Row, Col } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 function Header() {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        <img
-          alt=""
-          src="/icon.png"
-          width="60"
-          height="60"
-          className="d-inline-block align-top"
-        />
+      <Navbar.Brand className="px-3">
+        <Row>
+          <Col>
+            <img
+              alt=""
+              src="/icon.png"
+              width="60"
+              height="60"
+              className="d-inline-block align-top"
+            />
+          </Col>
+          <Col>
+            <div style={{ color: 'white' }}>
+              Come On!
+              <br />
+              Impacting Asteroid
+            </div>
+          </Col>
+        </Row>
       </Navbar.Brand>
-      <div style={{ color: 'white' }}>
-        Come On!
-        <br />
-        Impacting Asteroid
-      </div>
+
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
@@ -41,7 +48,7 @@ function Header() {
             }
             style={{ textDecoration: 'none', color: 'gray' }}
           >
-            <h3 style={{ paddingLeft: 13 }}>探索/再描画</h3>
+            <h3 style={{ paddingLeft: 35 }}>探索/再描画</h3>
           </NavLink>
           <NavLink
             to="/"
@@ -51,7 +58,7 @@ function Header() {
             style={{ textDecoration: 'none', color: 'gray' }}
             onClick={(e) => e.preventDefault() /* do nothing. */}
           >
-            <h3 style={{ paddingLeft: 13 }}>再測定</h3>
+            <h3 style={{ paddingLeft: 35 }}>再測定</h3>
           </NavLink>
           <NavLink
             to="/Report"
@@ -60,7 +67,7 @@ function Header() {
             }
             style={{ textDecoration: 'none', color: 'gray' }}
           >
-            <h3 style={{ paddingLeft: 13 }}>レポート</h3>
+            <h3 style={{ paddingLeft: 35 }}>レポート</h3>
           </NavLink>
         </Nav>
       </Navbar.Collapse>
