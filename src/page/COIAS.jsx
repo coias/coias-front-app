1814ea3f6567fbc6f310e51626c0f6966b02b581
AnbaiAnbai <anbai@aizulab.com>
2022-06-30 16:01:36 +0900

    # fix : result bug fix

--- src/page/COIAS.jsx
+++ src/page/COIAS.jsx
@@ -245,7 +245,7 @@ function COIAS({
     const response = await axios.put(
       `${reactApiUri}AstsearchR_between_COIAS_and_ReCOIAS?num=${num}`,
     );
-    const redisp = await response.data[0].result;
+    const redisp = await response.data.result;
 
     // 選択を同期させるため、オブジェクトに変更
     const toObject = {};
