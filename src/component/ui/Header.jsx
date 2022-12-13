b37e81496dab4420207c34886d86b4f87b545975
塩野目 <shionome@aizulab.com>
2022-12-13 11:50:21 +0900

    #fix: ハンバーガーメニュー追加途中

--- src/component/ui/Header.jsx
+++ src/component/ui/Header.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { useCallback, useContext, useState } from 'react';
-import { Button, Col, Nav, Navbar, Row } from 'react-bootstrap';
+import { Button, Col, Nav, Navbar, Offcanvas, Row } from 'react-bootstrap';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { ModeStatusContext } from '../functional/context';
 import ConfirmationModal from './ConfirmationModal';
@@ -18,7 +18,7 @@ function Header({ setMenunames, setFileNames }) {
   );
 
   return (
-    <Navbar className="color-nav" style={{ margin: 0, padding: 0 }}>
+    <Navbar expand="md" className="color-nav" style={{ margin: 0, padding: 0 }}>
       <Navbar.Brand>
         <Row>
           <Col>
@@ -50,89 +50,112 @@ function Header({ setMenunames, setFileNames }) {
           </Col>
         </Row>
       </Navbar.Brand>
-
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav>
-          <NavLink
-            to="/"
-            className={(navData) =>
-              navData.isActive ? 'active' : 'not-active'
-            }
-          >
-            <h3>探索準備</h3>
-          </NavLink>
-          <NavLink
-            to="/COIAS"
-            className={(navData) =>
-              navData.isActive ? 'active' : 'not-active'
-            }
-            style={{
-              opacity: modeStatus.COIAS ? 1 : 0.3,
+      <Navbar.Offcanvas placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>
+            <Row>
+              <Row>
+                <div
+                  className="p-0 m-0"
+                  style={{ color: '#28297E', fontSize: '15px' }}
+                >
+                  Come On! Impacting ASteroid
+                </div>
+              </Row>
+              <Row>
+                <div
+                  className="p-0 m-0"
+                  style={{ color: '#28297E', fontSize: '25px' }}
+                >
+                  COIAS
+                </div>
+              </Row>
+            </Row>
+          </Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body id="basic-navbar-nav">
+          <Nav>
+            <NavLink
+              to="/"
+              className={(navData) =>
+                navData.isActive ? 'active' : 'not-active'
+              }
+            >
+              <h3 className="nav-content">探索準備</h3>
+            </NavLink>
+            <NavLink
+              to="/COIAS"
+              className={(navData) =>
+                navData.isActive ? 'active' : 'not-active'
+              }
+              style={{
+                opacity: modeStatus.COIAS ? 1 : 0.3,
+              }}
+              onClick={modeStatus.COIAS ? () => {} : (e) => e.preventDefault()}
+            >
+              <h3 className="nav-content">探索/再描画</h3>
+            </NavLink>
+            <NavLink
+              to="/ManualMeasurement"
+              className={(navData) =>
+                navData.isActive ? 'active' : 'not-active'
+              }
+              style={{
+                opacity: modeStatus.Manual ? 1 : 0.3,
+              }}
+              onClick={modeStatus.Manual ? () => {} : (e) => e.preventDefault()}
+            >
+              <h3 className="nav-content">手動測定/名前修正</h3>
+            </NavLink>
+            <NavLink
+              to="/Report"
+              className={(navData) =>
+                navData.isActive ? 'active' : 'not-active'
+              }
+              style={{
+                opacity: modeStatus.Report ? 1 : 0.3,
+              }}
+              onClick={modeStatus.Report ? () => {} : (e) => e.preventDefault()}
+            >
+              <h3 className="nav-content">レポート</h3>
+            </NavLink>
+          </Nav>
+          <Button
+            onClick={() => {
+              setShow(true);
             }}
-            onClick={modeStatus.COIAS ? () => {} : (e) => e.preventDefault()}
+            variant="light"
+            disabled={checkIsStatusUpdated()}
           >
-            <h3>探索/再描画</h3>
-          </NavLink>
-          <NavLink
-            to="/ManualMeasurement"
-            className={(navData) =>
-              navData.isActive ? 'active' : 'not-active'
-            }
-            style={{
-              opacity: modeStatus.Manual ? 1 : 0.3,
+            探索終了
+          </Button>
+          <ConfirmationModal
+            show={show}
+            onHide={() => {
+              setShow(false);
             }}
-            onClick={modeStatus.Manual ? () => {} : (e) => e.preventDefault()}
-          >
-            <h3>手動測定/名前修正</h3>
-          </NavLink>
-          <NavLink
-            to="/Report"
-            className={(navData) =>
-              navData.isActive ? 'active' : 'not-active'
-            }
-            style={{
-              opacity: modeStatus.Report ? 1 : 0.3,
+            onClickYes={() => {
+              setFileNames(['ファイルを選択してください']);
+              setModeStatus({
+                COIAS: false,
+                Manual: false,
+                Report: false,
+              });
+              handleNavigate();
+              setShow(false);
+              setMenunames((prevMenunames) =>
+                prevMenunames.map((items) => {
+                  const objCopy = { ...items };
+                  objCopy.done = false;
+                  return objCopy;
+                }),
+              );
             }}
-            onClick={modeStatus.Report ? () => {} : (e) => e.preventDefault()}
-          >
-            <h3>レポート</h3>
-          </NavLink>
-        </Nav>
-      </Navbar.Collapse>
-      <Button
-        onClick={() => {
-          setShow(true);
-        }}
-        variant="light"
-        disabled={checkIsStatusUpdated()}
-      >
-        探索終了
-      </Button>
-      <ConfirmationModal
-        show={show}
-        onHide={() => {
-          setShow(false);
-        }}
-        onClickYes={() => {
-          setFileNames(['ファイルを選択してください']);
-          setModeStatus({
-            COIAS: false,
-            Manual: false,
-            Report: false,
-          });
-          handleNavigate();
-          setShow(false);
-          setMenunames((prevMenunames) =>
-            prevMenunames.map((items) => {
-              const objCopy = { ...items };
-              objCopy.done = false;
-              return objCopy;
-            }),
-          );
-        }}
-        confirmMessage="状態を全てクリアしますか？"
-      />
+            confirmMessage="状態を全てクリアしますか？"
+          />
+        </Offcanvas.Body>
+      </Navbar.Offcanvas>
     </Navbar>
   );
 }
