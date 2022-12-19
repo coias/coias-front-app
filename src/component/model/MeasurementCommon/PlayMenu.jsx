c70958d1b26f9061b8c94ddf33a863af1273be20
AnbaiAnbai <anbai@aizulab.com>
2022-12-19 10:41:23 +0900

    #fix : 操作系ボタン群のレスポンシブ対応

--- src/component/model/MeasurementCommon/PlayMenu.jsx
+++ src/component/model/MeasurementCommon/PlayMenu.jsx
@@ -3,8 +3,6 @@ import React, { useCallback, useContext, useEffect, useState } from 'react';
 import {
   Button,
   ButtonGroup,
-  Col,
-  Container,
   Form,
   Nav,
   Navbar,
@@ -211,241 +209,232 @@ function PlayMenu({
 
   return (
     <Navbar bg="light" className="play-menu">
-      <Container fluid>
-        <Col md={4} sm={6}>
-          <Nav>
-            <Nav.Item className="text-center d-flex m-1">
-              <Button
-                className="blink-button"
-                variant="light"
-                onClick={() => {
-                  if (!play) {
-                    onClickBlinkStart(imageNames);
-                  } else {
-                    onClickBlinkStop();
-                  }
-                }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{ color: CONSTANT.defaultBtnColor }}
-                >
-                  {play ? (
-                    <FaStop size={CONSTANT.iconSize} />
-                  ) : (
-                    <FaPlay size={CONSTANT.iconSize} />
-                  )}
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-            <Nav.Item className="text-center d-flex m-0">
-              <Button
-                variant="light"
-                onClick={() => {
-                  onClickBack();
-                }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{ color: CONSTANT.defaultBtnColor }}
-                >
-                  <FaStepBackward size={CONSTANT.iconSize} />
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-            <Nav.Item className="text-center d-flex m-0">
-              <Button
-                variant="light"
-                onClick={() => {
-                  onClickNext();
-                }}
-                style={{ marginLeft: '-10px' }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{ color: CONSTANT.defaultBtnColor }}
-                >
-                  <FaStepForward size={CONSTANT.iconSize} />
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-            <Nav.Item className="d-flex">
-              <Form.Control
-                as="select"
-                defaultValue="250"
-                style={{ minWidth: '60px' }}
-                onChange={(v) => {
-                  setSec(parseFloat(v.target.value));
-                }}
-              >
-                <option value="10">0.01</option>
-                <option value="20">0.02</option>
-                <option value="50">0.05</option>
-                <option value="100">0.10</option>
-                <option value="250">0.25</option>
-                <option value="500">0.50</option>
-              </Form.Control>
-              <Form.Text style={{ margin: 'auto 0', marginLeft: '5px' }}>
-                sec
-              </Form.Text>
-            </Nav.Item>
-            <Nav.Item className="text-center d-flex m-0">
-              <Button
-                variant="light"
-                onClick={() => {
-                  onClickZoomIn();
-                }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{ color: CONSTANT.defaultBtnColor }}
-                >
-                  <BiZoomIn size={CONSTANT.iconSize} />
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-            <Nav.Item className="text-center d-flex m-0">
-              <Button
-                variant="light"
-                onClick={() => {
-                  onClickZoomOut();
-                }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{ color: CONSTANT.defaultBtnColor }}
-                >
-                  <BiZoomOut size={CONSTANT.iconSize} />
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-            <Nav.Item className="text-center d-flex m-0">
-              <Button
-                data-active={isHide}
-                variant="light"
-                onClick={() => {
-                  setIsHide(!isHide);
-                }}
-              >
-                <IconContext.Provider
-                  // eslint-disable-next-line react/jsx-no-constructed-context-values
-                  value={{
-                    color: isHide
-                      ? CONSTANT.selectedBtnColor
-                      : CONSTANT.defaultBtnColor,
-                  }}
-                >
-                  <BiHide size={CONSTANT.iconSize} />
-                </IconContext.Provider>
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Col>
-        <Col md={8} className="d-flex">
-          <ButtonGroup className="flex-grow-1" style={{ margin: 'auto 0' }}>
-            {imageNames
-              .filter((img) => img.visible)
-              .map((name) => (
-                <ToggleButton
-                  id={`radio-${
+      <Nav>
+        <Nav.Item className="text-center d-flex m-1">
+          <Button
+            className="blink-button"
+            variant="light"
+            onClick={() => {
+              if (!play) {
+                onClickBlinkStart(imageNames);
+              } else {
+                onClickBlinkStop();
+              }
+            }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{ color: CONSTANT.defaultBtnColor }}
+            >
+              {play ? (
+                <FaStop size={CONSTANT.iconSize} />
+              ) : (
+                <FaPlay size={CONSTANT.iconSize} />
+              )}
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+        <Nav.Item className="text-center d-flex m-0">
+          <Button
+            variant="light"
+            onClick={() => {
+              onClickBack();
+            }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{ color: CONSTANT.defaultBtnColor }}
+            >
+              <FaStepBackward size={CONSTANT.iconSize} />
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+        <Nav.Item className="text-center d-flex m-0">
+          <Button
+            variant="light"
+            onClick={() => {
+              onClickNext();
+            }}
+            style={{ marginLeft: '-10px' }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{ color: CONSTANT.defaultBtnColor }}
+            >
+              <FaStepForward size={CONSTANT.iconSize} />
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+        <Nav.Item className="d-flex">
+          <Form.Control
+            as="select"
+            defaultValue="250"
+            style={{ minWidth: '60px' }}
+            onChange={(v) => {
+              setSec(parseFloat(v.target.value));
+            }}
+          >
+            <option value="10">0.01</option>
+            <option value="20">0.02</option>
+            <option value="50">0.05</option>
+            <option value="100">0.10</option>
+            <option value="250">0.25</option>
+            <option value="500">0.50</option>
+          </Form.Control>
+          <Form.Text style={{ margin: 'auto 0', marginLeft: '5px' }}>
+            sec
+          </Form.Text>
+        </Nav.Item>
+        <Nav.Item className="text-center d-flex m-0">
+          <Button
+            variant="light"
+            onClick={() => {
+              onClickZoomIn();
+            }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{ color: CONSTANT.defaultBtnColor }}
+            >
+              <BiZoomIn size={CONSTANT.iconSize} />
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+        <Nav.Item className="text-center d-flex m-0">
+          <Button
+            variant="light"
+            onClick={() => {
+              onClickZoomOut();
+            }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{ color: CONSTANT.defaultBtnColor }}
+            >
+              <BiZoomOut size={CONSTANT.iconSize} />
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+        <Nav.Item className="text-center d-flex m-0">
+          <Button
+            data-active={isHide}
+            variant="light"
+            onClick={() => {
+              setIsHide(!isHide);
+            }}
+          >
+            <IconContext.Provider
+              // eslint-disable-next-line react/jsx-no-constructed-context-values
+              value={{
+                color: isHide
+                  ? CONSTANT.selectedBtnColor
+                  : CONSTANT.defaultBtnColor,
+              }}
+            >
+              <BiHide size={CONSTANT.iconSize} />
+            </IconContext.Provider>
+          </Button>
+        </Nav.Item>
+      </Nav>
+      <ButtonGroup className="flex-grow-1" style={{ margin: 'auto 0' }}>
+        {imageNames
+          .filter((img) => img.visible)
+          .map((name) => (
+            <ToggleButton
+              id={`radio-${
+                parseInt(
+                  name.name.substr(0, 1) === '0'
+                    ? name.name.substr(1, 1)
+                    : name.name.substr(0, 2),
+                  10,
+                ) - 1
+              }`}
+              type="radio"
+              variant="outline-secondary"
+              name="radio"
+              checked={
+                (radioValue === name.name &&
+                  currentPage ===
                     parseInt(
                       name.name.substr(0, 1) === '0'
                         ? name.name.substr(1, 1)
                         : name.name.substr(0, 2),
                       10,
-                    ) - 1
-                  }`}
-                  type="radio"
-                  variant="outline-secondary"
-                  name="radio"
-                  checked={
-                    (radioValue === name.name &&
-                      currentPage ===
-                        parseInt(
-                          name.name.substr(0, 1) === '0'
-                            ? name.name.substr(1, 1)
-                            : name.name.substr(0, 2),
-                          10,
-                        ) -
-                          1) ||
-                    currentPage ===
-                      parseInt(
-                        name.name.substr(0, 1) === '0'
-                          ? name.name.substr(1, 1)
-                          : name.name.substr(0, 2),
-                        10,
-                      ) -
-                        1
-                  }
-                  onChange={(e) => {
-                    setCurrentPage(
-                      parseInt(
-                        name.name.substr(0, 1) === '0'
-                          ? name.name.substr(1, 1)
-                          : name.name.substr(0, 2),
-                        10,
-                      ) - 1,
-                    );
-                    setRadioValue(e.currentTarget.value);
-                  }}
-                  bsstyle="default"
-                  style={{ fontWeight: 'bold', textAlign: 'center' }}
-                >
-                  {name.name.substr(0, 1) === '0'
-                    ? name.name.substr(1, 1)
-                    : name.name.substr(0, 2)}
-                </ToggleButton>
-              ))}
-          </ButtonGroup>
-          <ButtonGroup
-            className="justify-content-end"
-            style={{ marginLeft: '5rem', marginRight: '10px' }}
+                    ) -
+                      1) ||
+                currentPage ===
+                  parseInt(
+                    name.name.substr(0, 1) === '0'
+                      ? name.name.substr(1, 1)
+                      : name.name.substr(0, 2),
+                    10,
+                  ) -
+                    1
+              }
+              onChange={(e) => {
+                setCurrentPage(
+                  parseInt(
+                    name.name.substr(0, 1) === '0'
+                      ? name.name.substr(1, 1)
+                      : name.name.substr(0, 2),
+                    10,
+                  ) - 1,
+                );
+                setRadioValue(e.currentTarget.value);
+              }}
+              bsstyle="default"
+              style={{ fontWeight: 'bold', textAlign: 'center' }}
+            >
+              {name.name.substr(0, 1) === '0'
+                ? name.name.substr(1, 1)
+                : name.name.substr(0, 2)}
+            </ToggleButton>
+          ))}
+      </ButtonGroup>
+      <ButtonGroup style={{ marginRight: '10px' }}>
+        <Button variant="light" onClick={() => setSettingModalShow(true)}>
+          <IconContext.Provider
+            // eslint-disable-next-line react/jsx-no-constructed-context-values
+            value={{ color: CONSTANT.defaultBtnColor }}
           >
-            <Button variant="light" onClick={() => setSettingModalShow(true)}>
-              <IconContext.Provider
-                // eslint-disable-next-line react/jsx-no-constructed-context-values
-                value={{ color: CONSTANT.defaultBtnColor }}
-              >
-                <AiFillSetting size={CONSTANT.iconSize} />
-              </IconContext.Provider>
-            </Button>
-            <SettingModal
-              show={settingModalShow}
-              onHide={() => {
-                setSettingModalShow(false);
-                // setImageURLs(JSON.parse(JSON.stringify(imageNames)));
-                setSetting(true);
-                const array = setValid();
-                setCurrentPage(array[0]);
-                setIndex(0);
-              }}
-              title="表示設定"
-              imageURLs={imageNames}
-              setDefaultZoomRate={setDefaultZoomRate}
-              defaultZoomRate={defaultZoomRate}
-              setIsAutoSave={setIsAutoSave}
-              isAutoSave={isAutoSave}
-              disableShowAutoSave={disableShowAutoSave}
-            />
-            <Button variant="light" onClick={() => setHelpModalShow(true)}>
-              <IconContext.Provider
-                // eslint-disable-next-line react/jsx-no-constructed-context-values
-                value={{ color: CONSTANT.defaultBtnColor }}
-              >
-                <BiHelpCircle size={CONSTANT.iconSize} />
-              </IconContext.Provider>
-            </Button>
-            <HelpModal
-              show={helpModalShow}
-              onHide={() => {
-                setHelpModalShow(false);
-              }}
-              title="ヘルプ"
-              imageURLs={imageNames}
-            />
-          </ButtonGroup>
-        </Col>
-      </Container>
+            <AiFillSetting size={CONSTANT.iconSize} />
+          </IconContext.Provider>
+        </Button>
+        <SettingModal
+          show={settingModalShow}
+          onHide={() => {
+            setSettingModalShow(false);
+            // setImageURLs(JSON.parse(JSON.stringify(imageNames)));
+            setSetting(true);
+            const array = setValid();
+            setCurrentPage(array[0]);
+            setIndex(0);
+          }}
+          title="表示設定"
+          imageURLs={imageNames}
+          setDefaultZoomRate={setDefaultZoomRate}
+          defaultZoomRate={defaultZoomRate}
+          setIsAutoSave={setIsAutoSave}
+          isAutoSave={isAutoSave}
+          disableShowAutoSave={disableShowAutoSave}
+        />
+        <Button variant="light" onClick={() => setHelpModalShow(true)}>
+          <IconContext.Provider
+            // eslint-disable-next-line react/jsx-no-constructed-context-values
+            value={{ color: CONSTANT.defaultBtnColor }}
+          >
+            <BiHelpCircle size={CONSTANT.iconSize} />
+          </IconContext.Provider>
+        </Button>
+        <HelpModal
+          show={helpModalShow}
+          onHide={() => {
+            setHelpModalShow(false);
+          }}
+          title="ヘルプ"
+          imageURLs={imageNames}
+        />
+      </ButtonGroup>
     </Navbar>
   );
 }
