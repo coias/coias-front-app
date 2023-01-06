91566e9264def005ca298d86c56cd59b35b0ed6d
塩野目 <shionome@aizulab.com>
2023-01-06 11:18:19 +0900

    #fix: ManualStarModalスタイル修正

--- src/component/model/ManualMeasurement/ManualStarModal.jsx
+++ src/component/model/ManualMeasurement/ManualStarModal.jsx
@@ -274,7 +274,10 @@ function ManualStarModal({
         onExited={onExited}
       >
         <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            style={{ color: '#5c636a', fontWeight: 'bold' }}
+          >
             {`#H${'000000'.slice(
               (leadStarNumber + activeKey).toString().length - 6,
             )}${leadStarNumber + activeKey}, ${
@@ -307,6 +310,7 @@ function ManualStarModal({
               drawImage();
               setCanvasManualRectanglCoordinates([]);
             }}
+            className="btn-style box_border_blue"
           >
             やり直す
           </Button>
@@ -322,6 +326,7 @@ function ManualStarModal({
               setCenterCoodinate(null);
               setCanvasManualRectanglCoordinates([]);
             }}
+            className="btn-style box_blue"
           >
             完了
           </Button>
